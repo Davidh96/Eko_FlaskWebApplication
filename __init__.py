9acedea4c095da625c7b1107792a3cb9cb0a14ef
david <c14464428@mydit.ie>
2018-02-08 14:34:28 +0000

    Added haversine formula function

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import jsonify
 from firebase import firebase
-
+from math import radians, sin, cos, sqrt, atan2
 
 app = Flask(__name__)
-firebase = firebase.FirebaseApplication('',None)
+firebase = firebase.FirebaseApplication('https://ekoapp-ba9b5.firebaseio.com/',None)
 
 #displays all events
 @app.route('/')
@@ -42,11 +42,27 @@ def removeUnicode(formatedJSON):
 			formatedJSON = formatedJSON.replace(unicodeChar,"'")
 			return formatedJSON
 
+def haversine(userLat,userLong,eventLat,eventLong):
+	radius = 6372.8 #earths radius(km)
+	#haversine formulae
+	a = sin(radians(eventLat-userLat)/2)**2 + cos(radians(userLat)) * cos(radians(eventLat)) * sin(radians(eventLong-userLong)/2)**2
+	b = 2*atan2(sqrt(a),sqrt(1-a))
+	return b*radius
+
 #displays select events
 @app.route('/retrieveEvents/<id>')
 def retrieveEvents(id):
 	result= firebase.get('/events/'+id,None)
 	return removeUnicode(str(result))
+
+@app.route('/getEvents/<location>')
+def getEvents(location):
+	parsePoint = ','
+	parseLocation = location.rfind(parsePoint)
+	latitude=location[:parseLocation]
+	longitude=location[parseLocation+1:]
+
+	return str(haversine(35,-6,35.1,-6.1))
 
 #page used for testing
 @app.route('/hello/<name>')
